33ba5bb873968dea1f8bb5e1ae0817d10ee1a0b8
Abhishek Yadav <abhishekyad1098@gmail.com>
2018-09-08 02:16:26 -0400

    it works

--- Hand.py
+++ Hand.py
@@ -5,9 +5,12 @@ class Hand:
     def __init__(self):
         self.fingers = 1
 
+    def get_amount_fingers(self):
+        return self.fingers
+
     def add_fingers(self, hand):
         if self.fingers != 0:
-            self.fingers += hand.fingers
+            self.fingers += hand.get_amount_fingers()
 
 
 class Player:
@@ -21,6 +24,38 @@ class Player:
 
     def hit(self, side, opponentHand):
         if side == 'left':
-            self.leftHand.addFingers(opponentHand.fingers)
+            self.leftHand.add_fingers(opponentHand)
         else:
-            self.rightHand.addFingers(opponentHand.fingers)
+            self.rightHand.add_fingers(opponentHand)
+
+
+player1 = Player()
+player2 = Player()
+
+print('Opponent Left: ', player1.leftHand.fingers, '\t', 'Opponent Right: ', player1.rightHand.fingers)
+print('Your Left: ', player2.leftHand.fingers, '\t', 'Your Right: ', player2.rightHand.fingers)
+
+game = False
+move = None
+
+player1.hit('left', player2.rightHand)
+
+
+print('Opponent Left: ', player1.leftHand.fingers, '\t', 'Opponent Right: ', player1.rightHand.fingers)
+print('Your Left: ', player2.leftHand.fingers, '\t', 'Your Right: ', player2.rightHand.fingers)
+
+
+
+while(game==True):
+    print('Opponent Left: ', player1.leftHand.fingers, '\t', 'Opponent Right: ', player1.rightHand.fingers)
+    print('Your Left: ', player2.leftHand.fingers, '\t', 'Your Right: ', player2.rightHand.fingers)
+    move = input()
+    if move=='hit':
+        print('choose hand')
+        sideUser = input()
+        print('choose hand to hit')
+        sideCPU = input()
+        if sideCPU=='left':
+            player2.hit(sideUser, player1.leftHand)
+        else:
+            player2.hit(sideUser, player1.rightHand)
