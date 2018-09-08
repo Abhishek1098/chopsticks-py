1e9fad8eeeb8f70b17dcb64c90c12973afa8b09d
Abhishek Pandya <abhishekp213@hotmail.com>
2018-09-07 22:12:05 -0400

    add a person

---
+++ Person.py
@@ -0,0 +1,12 @@
+class Player():
+    leftHand
+    rightHand
+    isUser
+
+    def hit(side, opponentHand):
+        if side == 'left':
+            leftHand.addFingers(opponentHand)
+        else
+            rightHand.addFingers(opponentHand)
+
+
