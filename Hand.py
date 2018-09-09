0f533ca6aff9ad6faad761417ee8f086a5099ee2
Abhishek Pandya <abhishekp213@hotmail.com>
2018-09-09 01:47:24 -0400

    node has ai from scratch, but it's not finished and not used.

---
+++ Hand.py
@@ -0,0 +1,111 @@
+class Hand:
+
+    fingers = 1
+
+    def __init__(self):
+        self.fingers = 1
+
+    def add_fingers(self, hand):
+        if self.fingers != 0:
+            self.fingers += hand.fingers
+        self.check_fingers()
+        hand.check_fingers()
+    def check_fingers(self):
+        if self.fingers >= 5:
+            self.fingers = 0
+
+class Player:
+
+    leftHand = Hand()
+    rightHand = Hand()
+
+    def __init__(self):
+        self.leftHand = Hand()
+        self.rightHand = Hand()
+
+    def hit(self, side, opponentHand):
+        if side == 'left':
+            self.leftHand.add_fingers(opponentHand)
+        else:
+            self.rightHand.add_fingers(opponentHand)
+
+import random
+
+player1 = Player()
+player2 = Player()
+
+print('Opponent Left: ', player1.leftHand.fingers, '\t', 'Opponent Right: ', player1.rightHand.fingers)
+print('Your Left: ', player2.leftHand.fingers, '\t', 'Your Right: ', player2.rightHand.fingers)
+
+game = True
+move = None
+user_turn = True
+
+print('Opponent Left: ', player1.leftHand.fingers, '\t', 'Opponent Right: ', player1.rightHand.fingers)
+print('Your Left: ', player2.leftHand.fingers, '\t', 'Your Right: ', player2.rightHand.fingers)
+
+def left_or_right():
+    if random.randint(0,1) > 0:
+        return 'left'
+    else:
+        return 'right'
+
+while(game==True):
+    print('******************************')
+    if user_turn:
+        print('Opponent Left: ', player1.leftHand.fingers, '\t', 'Opponent Right: ', player1.rightHand.fingers)
+        print('Your Left: ', player2.leftHand.fingers, '\t', 'Your Right: ', player2.rightHand.fingers)
+        print('choose move')
+        move = input()
+        if move=='hit':
+            print('choose hand')
+            sideUser = input()
+            print('choose hand to hit')
+            sideCPU = input()
+            if sideCPU=='left':
+                player1.hit(sideUser, player2.leftHand)
+            else:
+                player1.hit(sideUser, player2.rightHand)
+    else:
+        print('Opponent Left: ', player1.leftHand.fingers, '\t', 'Opponent Right: ', player1.rightHand.fingers)
+        print('Your Left: ', player2.leftHand.fingers, '\t\t', '\t  Your Right: ', player2.rightHand.fingers)
+        print('choose move')
+        move = 'hit'
+        print(move)
+        if move=='hit':
+            print('choose hand')
+            sideUser = left_or_right()
+            print(sideUser)
+            print('choose hand to hit')
+            sideCPU = left_or_right()
+            print(sideCPU)
+            if sideCPU=='left':
+                player2.hit(sideUser, player1.leftHand)
+            else:
+                player2.hit(sideUser, player1.rightHand)
+                
+    if user_turn == True:
+        user_turn = False
+    else:
+        user_turn = True
+
+class nn:
+    
+
+class ai_cpu:
+     ai_player
+     decision_move
+     decision_side
+     decision_attack_side
+     weights
+
+     def __init__(self):
+        self.ai_player = Player()
+
+     def calculate_move(self, user_left, user_right, cpu_left, cpu_right):
+        # it needs 4 input values: fingers its left and right hands, and fingers on opponent's hands
+        cpu_left
+
+    def generate_first_pop():
+        for x in range(3)
+            weights.append(random.uniform())
--- Node.js
+++ Node.js
@@ -1,23 +1,135 @@
-const tf = require('@tensorflow/tfjs');
 
-// Load the binding:
-require('@tensorflow/tfjs-node');  // Use '@tensorflow/tfjs-node-gpu' if running with GPU.
+// Make a simple model
+class NeuralNetwork
+{
+  // initialize network here
+  constructor()
+  {
+    this.network = [new Layer(0,0,true), new Layer(4,4,false), new Layer(4,5,false)];
+  }
+  // 4, 4, 5 nodes in respective layers
 
-// Train a simple model:
-const model = tf.sequential();
-model.add(tf.layers.dense({units: 100, activation: 'relu', inputShape: [10]}));
-model.add(tf.layers.dense({units: 1, activation: 'linear'}));
-model.compile({optimizer: 'sgd', loss: 'meanSquaredError'});
-
-const xs = tf.randomNormal([100, 10]);
-const ys = tf.randomNormal([100, 1]);
-
-model.fit(xs, ys, {
-  epochs: 100,
-  callbacks: {
-    onEpochEnd: async (epoch, log) => {
-      console.log(`Epoch ${epoch}: loss = ${log.loss}`);
+  //feed forward - calculate move
+  feedForward()
+  {
+    //loop through each layer
+    //j is the layer you are in
+    for (var j = 1; j < network.length; j++)
+    {
+      // multiply prev layer's activations by weights
+      network[j].activation_values = network[j].weights.multiply(network[j-1].activation_values);
+      network[j].activation_values = relu(network[j].activation_values)
+      for (var i =0 ; i < network[j].activation_values.length; i++)
+      {
+        console.log(etwork[j].activation_values[i]);
+      }
     }
   }
-});
+
+  relu(activation_values)
+  {
+    for (var i = 0; i < activation_values.length; i++)
+    {
+      activation_values[i] = x;
+      if (x < -10) {
+        activation_values[i] = 0;
+      } else if (x > 10) {
+        activation_values[i] = 1;
+      } else {
+        activation_values[i] = 1 / (1 + Math.exp(-x));
+      }
+    }
+  }
+}
+
+class Layer
+{
+  
+  constructor(nodesPrevLayer,nodesThisLayer, isFirstLayer)
+  {
+    this.activation_values = [];
+    if (isFirstLayer == true)
+    {
+      activation_values = [2, 3, 4, 1];
+    } else {
+      weights = new Matrix(nodesPrevLayer,nodesThisLayer);
+      weights.generate();
+    }
+  }
+  
+}
+
+class Matrix
+{
+  constructor (rows, cols)
+  {
+    // var m =  new Matrix(2,3)
+    this.rows = rows;
+    this.cols = cols;
+    this.matrix = [];
+
+    for (var i = 0; i < this.rows; i++)
+    {
+      this.matrix[i] = [];
+      for (var j = 0; j < this.cols; j++)
+      {
+        this.matrix[i][j] = 0;
+      }
+    }
+  }
+
+  print()
+  {
+    for (var i = 0; i < this.rows; i++)
+    {
+      for (var j = 0; j < this.cols; j++)
+      {
+        console.log(this.matrix[i][j]);
+      }
+      console.log()
+    }
+  }
+
+  generate()
+  {
+    for (var i = 0; i < this.rows; i++)
+    {
+      for (var j = 0; j < this.cols; j++)
+      {
+        this.matrix[i][j] = Math.random();
+      }
+    }
+  }
+
+  mutate()
+  {
+    var i = Math.floor(Math.random() * this.rows);
+    var j = Math.floor(Math.random() * this.cols);
+    this.matrix[i][j] = Math.random()*10;  
+  }
+
+  multiply(n)
+  {
+    let result = new Matrix(this.rows, n.cols)
+    for (let i = 0; i < result.rows; i++)
+    {
+      let a = this;
+      let b = n;
+
+      for (let j = 0; j < result.cols; j++)
+      {
+        for (var k = 0; k < a.cols; k++)
+        {
+          result.matrix[i][j] += a.matrix[i][k] * b.matrix[k][j];
+        }
+      }
+    }
+    return result;
+  }
+}
+
+nn = new NeuralNetwork();
+nn.feedForward();
+
+
      
